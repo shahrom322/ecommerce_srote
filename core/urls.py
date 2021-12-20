62d32f6b2ca5e44398ea2dcf09599cc03a9cc24b
shahrom322 <ololoev05@gmail.com>
2021-12-20 19:37:21 +0300

    Init commit

---
+++ core/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+
+from core.views import checkout, HomeView, ItemDetailView, add_item_to_cart, remove_from_cart
+
+app_name = 'core'
+
+urlpatterns = [
+    path('', HomeView.as_view(), name='home'),
+    path('checkout/', checkout, name='checkout'),
+    path('product/<slug>/', ItemDetailView.as_view(), name='product'),
+    path('add-to-cart/<slug>/', add_item_to_cart, name='add-to-cart'),
+    path('remove-from-cart/<slug>/', remove_from_cart, name='remove-from-cart'),
+]
