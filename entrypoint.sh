9c2b0ebadf046709ece4bd786a02fda5c834e732
shahrom322 <ololoev05@gmail.com>
2022-01-03 18:53:00 +0300

    Добавлена кофигурация nginx

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,16 +2,13 @@
 
 if [ "$DATABASE" = "postgres" ]
 then
-  echo "Waiting for postgres..."
+    echo "Waiting for postgres..."
 
-  while [ ! nc -z $SQL_HOST $SQL_PORT ]; do
-    sleep 0.1
-  done
+    while ! nc -z $SQL_HOST $SQL_PORT; do
+      sleep 0.1
+    done
 
-  echo "PostgreSQL started"
+    echo "PostgreSQL started"
 fi
-
-python manage.py flush --no-input
-python manage.py migrate --no-input
 
 exec "$@"
